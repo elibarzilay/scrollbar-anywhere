6bcbb65964d83662ec22a1804c64d2802e5cf40f
Eli Barzilay <eli@barzilay.org>
2017-10-02 05:48:46 -0400

    More improvements
    * Make movement sensitivity configurable.
    
    * Better HTML highlighting, and keyboard controls.
    
    * Improve JS code.

--- config.js
+++ config.js
@@ -1,43 +1,51 @@
 "use strict";
 
-let getElt = document.getElementById.bind(document);
+const getElt = document.getElementById.bind(document);
 
-function forallOptions(cb) {
-  for (let k in options) {
-    let inp = getElt(k);
+const forallOptions = cb => {
+  for (const k in options) {
+    const inp = getElt(k);
     if (inp) cb(k, inp); // check in case of junk in settings
   }
-}
+};
 
-let isBoolOpt = (o) => BOOLEAN_OPTS.indexOf(o) >= 0;
+const isBoolOpt = (o) => BOOLEAN_OPTS.includes(o);
 
-function save() {
+const save = () => {
   forallOptions((k, inp) =>
     options[k] = isBoolOpt(k) ? inp.checked : Number(inp.value));
   chrome.storage.sync.set({options});
-}
+};
 
-function load() {
+const load = () => {
   forallOptions((k, inp) => {
     if (isBoolOpt(k)) inp.checked = options[k]; else inp.value = options[k];
   });
-}
+};
 
-let updateTimeoutId;
+let updateTimeoutId = null;
 
-function onUpdate(ev) {
+const onUpdate = ev => {
   if (updateTimeoutId != null) clearTimeout(updateTimeoutId);
   updateTimeoutId = setTimeout(save, 200);
-}
+};
 
-function start() {
-  forallOptions((k, inp) => inp.addEventListener("change", onUpdate, false));
-  let platform = (s) => navigator.platform.startsWith(s);
+const toggleBoolean = ev => {
+  if (![" ", "Enter"].includes(ev.key)) return;
+  ev.target.click();
+  blockEvent(ev);
+};
+
+const start = () => {
+  forallOptions((k, inp) => {
+    inp.addEventListener("change", onUpdate, false);
+    if (isBoolOpt(k)) inp.addEventListener("keyup", toggleBoolean, true); });
+  const platform = (s) => navigator.platform.startsWith(s);
   getElt("metaKey").nextSibling.innerHTML =
     platform("Win") ? "&#x229E;" : platform("Mac") ? "&#x2318;" : "Meta";
-  let text = ["<i>Here's a bunch of text to scroll:</i>", ""];
+  const text = ["<i>Here's a bunch of text to scroll:</i>", ""];
   let beers = 99;
-  let bottles = (n, rest) =>
+  const bottles = (n, rest) =>
     `${n==0 ? "no more" : n} bottle${n==1?"":"s"} of beer${rest}`;
   while (beers > 0) {
     text.push(bottles(beers, " on the wall,"),
@@ -47,14 +55,14 @@ function start() {
               "");
   }
   getElt("long_footer").innerHTML = text.join("<br>");
-}
+};
 
-function loadAndStart(ev) {
+const loadAndStart = ev => {
   chrome.storage.sync.get("options", val => {
     if (val && val.options) Object.assign(options, val.options);
     load();
     start(); });
-}
+};
 
 document.addEventListener("DOMContentLoaded", loadAndStart, true);
 
--- main.js
+++ main.js
@@ -2,29 +2,30 @@
 
 // ===== Options =====
 
-let options = { // defaults
+const options = { // defaults
   lButton: false, mButton: false, rButton: true,
   shiftKey: false, ctrlKey: false, altKey: false, metaKey: false,
-  speed: 20000, friction: 5,
+  sensitivity: 9, speed: 20000, friction: 5,
   notext: false,
   debug: false
 };
-let cOptions = {};
-let addComputedOptions = () =>
+const cOptions = {};
+const addComputedOptions = () =>
   Object.assign(cOptions, {buttons: (options.lButton ? 1 : 0) +
                                     (options.rButton ? 2 : 0) +
-                                    (options.mButton ? 4 : 0)});
+                                    (options.mButton ? 4 : 0),
+                           sensitivity2: Math.pow(options.sensitivity, 2)});
 addComputedOptions();
 const KEYS = Object.keys(options).filter(x => x.endsWith("Key"));
 const BOOLEAN_OPTS =
   Object.keys(options).filter(x => (typeof options[x]) === "boolean");
 
-function setOptions(o) {
+const setOptions = o => {
   if (o) { Object.assign(options, o);
            addComputedOptions();
            debug("Options:", options);
            debug("Computed Options:", cOptions); }
-}
+};
 chrome.storage.onChanged.addListener((changes, _) =>
   setOptions(changes.options && changes.options.newValue));
 chrome.storage.sync.get("options", val => setOptions(val && val.options));
@@ -32,38 +33,37 @@ chrome.storage.sync.get("options", val => setOptions(val && val.options));
 // ===== Utilities =====
 
 // Debugging
-function debug(str, ...args) {
+const debug = (str, ...args) => {
   if (!options.debug) return;
   console.debug(`D2S: ${str}`, ...(args.map(x =>
     (typeof x === "function" && x.length == 0) ? x() : x)));
 }
 
 // Vector math
-let vadd  = (a,b) => [a[0]+b[0], a[1]+b[1]];
-let vsub  = (a,b) => [a[0]-b[0], a[1]-b[1]];
-let vmul  = (v,s) => [s*v[0], s*v[1]];
-let vdiv  = (v,s) => [v[0]/s, v[1]/s];
-let vmag2 = (v)   => v[0]*v[0] + v[1]*v[1];
-let vmag  = (v)   => Math.hypot(v[0], v[1]);
+const vadd  = (a,b) => [a[0]+b[0], a[1]+b[1]];
+const vsub  = (a,b) => [a[0]-b[0], a[1]-b[1]];
+const vmul  = (v,s) => [s*v[0], s*v[1]];
+const vdiv  = (v,s) => [v[0]/s, v[1]/s];
+const vmag2 = (v)   => v[0]*v[0] + v[1]*v[1];
+const vmag  = (v)   => Math.hypot(v[0], v[1]);
 
-let evPos = (ev) => [ev.clientX, ev.clientY];
+const evPos = (ev) => [ev.clientX, ev.clientY];
 
 // Completely block an event from going further
-function blockEvent(ev) {
+const blockEvent = ev => {
   ev.preventDefault();
   ev.stopImmediatePropagation();
 }
 
 // Test if the given point is directly over text
-let testElt = document.createElement("SPAN");
-function isOverText(ev) {
-  let parent = ev.target;
+const testElt = document.createElement("SPAN");
+const isOverText = ev => {
+  const parent = ev.target;
   if (parent == null) return false;
   for (let i = 0; i < parent.childNodes.length; i++) {
-    let child = parent.childNodes[i];
+    const child = parent.childNodes[i];
     if (child.nodeType !== Node.TEXT_NODE) continue;
     if (child.textContent.search(/\S/) == -1) continue;
-    // debug("TEXT_NODE: \"%s\"", child.textContent);
     try {
       testElt.appendChild(parent.replaceChild(testElt, child));
       if (testElt.isSameNode(document.elementFromPoint(ev.clientX, ev.clientY)))
@@ -82,14 +82,14 @@ function isOverText(ev) {
 // coordinates of the event are on the outside of a scrollable element.
 // The body element is treated separately since the visible size is
 // fetched differently depending on the doctype.
-function isOverScrollbar(ev) {
+const isOverScrollbar = ev => {
   let t = ev.target, d = document.documentElement;
   if (t == d) t = document.scrollingElement;
   if (t != document.scrollingElement)
     return isScrollable(t) &&
            (ev.offsetX - t.scrollLeft >= t.clientWidth ||
             ev.offsetY - t.scrollTop >= t.clientHeight);
-  let c =
+  const c =
     (d.scrollHeight == d.clientHeight && d.scrollHeight == d.offsetHeight)
     // true => guessing it's a no doctype document
       ? t : d;
@@ -100,16 +100,16 @@ function isOverScrollbar(ev) {
 // Can the given element be scrolled on either axis?
 // That is, is the scroll size greater than the client size
 // and the CSS overflow set to scroll or auto?
-function isScrollable(elt) {
-  let dv = document.defaultView;
-  let o = css => ["auto", "scroll"].indexOf(dv.getComputedStyle(elt)[css]) >= 0;
+const isScrollable = elt => {
+  const dv = document.defaultView;
+  const o = css => ["auto", "scroll"].includes(dv.getComputedStyle(elt)[css]);
   return (elt.scrollWidth  > elt.clientWidth  && o("overflow-x"))
       || (elt.scrollHeight > elt.clientHeight && o("overflow-y"));
 }
 
 // Return the first ancestor (or the element itself) that is scrollable
-function findScrollables(elt) {
-  let elts = [];
+const findScrollables = elt => {
+  const elts = [];
   while (true) {
     if (elt == document.documentElement) elt = document.scrollingElement;
     if (elt == null) break;
@@ -126,9 +126,9 @@ const TIME_STEP = 10, STOP = 0, CLICK = 1, DRAG = 2, GLIDE = 3;
 // Use this to change the cursor mostly, possibly also avoid events
 // being grabbed unpredictably.
 
-let CoverDiv = (() => {
+const CoverDiv = (() => {
   let elt = null;
-  function createCoverDiv() {
+  const createCoverDiv = () => {
     elt = document.createElement("div");
     Object.assign(elt.style, {
       background: "transparent none",
@@ -138,11 +138,11 @@ let CoverDiv = (() => {
       // backgroundColor: "rgba(255,128,255,0.5)",
     });
   }
-  function show() {
+  const show = () => {
     if (elt === null) createCoverDiv();
     document.body.appendChild(elt);
   }
-  function hide() {
+  const hide = () => {
     if (elt !== null && elt.parentNode !== null)
       elt.parentNode.removeChild(elt);
   }
@@ -151,7 +151,7 @@ let CoverDiv = (() => {
 
 // ===== Motion =====
 
-let Motion = (() => {
+const Motion = (() => {
   const FILTER_INTERVAL = 100;
   let position = null;
   let velocity = [0, 0];
@@ -159,14 +159,14 @@ let Motion = (() => {
   let impulses = {new: null, old: null};
   // set velocity and position, ensure it is within min and max values,
   // return true if there is motion
-  function clamp() {
-    let speed2 = vmag2(velocity);
+  const clamp = () => {
+    const speed2 = vmag2(velocity);
     if (speed2 < 1) { velocity = [0, 0]; return false; }
     if (speed2 > options.speed*options.speed)
       velocity = vmul(velocity, options.speed/vmag(velocity));
     return true;
   }
-  function start() {
+  const start = () => {
     if (impulses.new == impulses.old) { velocity = [0, 0]; return false; }
     position = impulses.new.pos;
     updateTime = impulses.new.time;
@@ -175,12 +175,12 @@ let Motion = (() => {
     return clamp();
   }
   // zero velocity
-  function stop() {
+  const stop = () => {
     impulses = {new: null, old: null};
     velocity = [0, 0];
   }
   // impulsively move to given position and time
-  function impulse(pos, time) {
+  const impulse = (pos, time) => {
     while (impulses.old != null && (time-impulses.old.time) > FILTER_INTERVAL)
       impulses.old = impulses.old.next;
     if (impulses.old == null)
@@ -189,43 +189,44 @@ let Motion = (() => {
       impulses.new = (impulses.new.next = {pos, time, next: null});
   }
   // update free motion to given time, return true there is motion
-  function glide(time) {
+  const glide = time => {
     impulses = {old: null, new: null};
     if (updateTime == null) { updateTime = time; return false; }
-    let dsecs = (time-updateTime)/1000;
-    let frictionMul = Math.max(1-(options.friction/FILTER_INTERVAL), 0);
-    frictionMul = Math.pow(frictionMul, dsecs*FILTER_INTERVAL);
+    const dsecs = (time - updateTime) / 1000;
+    const frictionMul =
+      Math.pow(Math.max(1 - (options.friction / FILTER_INTERVAL), 0),
+               dsecs*FILTER_INTERVAL);
     velocity = vmul(velocity, frictionMul);
-    let moving = clamp();
+    const moving = clamp();
     position = vadd(position, vmul(velocity, dsecs));
     updateTime = time;
     return moving;
   }
   //
-  let getPosition = () => position;
+  const getPosition = () => position;
   //
   return ({ start, stop, impulse, glide, getPosition });
 })();
 
-let Scroll = (() => {
+const Scroll = (() => {
   let elts = null, origins;
   // Start dragging
-  function start(ev) {
+  const start = ev => {
     elts = findScrollables(ev.target);
     origins = elts.map(elt => [elt.scrollLeft, elt.scrollTop]);
   }
   // Move the currently dragged elements relative to the starting position.
   // Return true the leftover movement vector (i.e., [0,0] if we scrolled,
   // >=1 if everything is scrolled with leftovers so we're at the edge).
-  function move(pos) {
+  const move = pos => {
     if (!elts) return false;
-    let [dx,dy] = pos;
+    let [dx, dy] = pos;
     for (let i = 0; i < elts.length; i++) {
-      let elt = elts[i], orig = origins[i];
+      const elt = elts[i], orig = origins[i];
       elt.scrollLeft = orig[0] - dx;
       elt.scrollTop  = orig[1] - dy;
-      dx -= orig[0]-elt.scrollLeft;
-      dy -= orig[1]-elt.scrollTop;
+      dx -= orig[0] - elt.scrollLeft;
+      dy -= orig[1] - elt.scrollTop;
       if (Math.abs(dx) < 1 && Math.abs(dy) < 1) return [0, 0];
     }
     return [Math.trunc(dx), Math.trunc(dy)];
@@ -238,36 +239,36 @@ let activity = STOP;
 let blockContextMenu = false;
 let mouseOrigin = null;
 
-function updateGlide() {
+const updateGlide = () => {
   if (activity != GLIDE) return;
   debug("glide update");
   if (Motion.glide(performance.now()) &&
-      Scroll.move(vsub(Motion.getPosition(),mouseOrigin)))
+      Scroll.move(vsub(Motion.getPosition(), mouseOrigin)))
     setTimeout(updateGlide, TIME_STEP);
   else stopGlide();
 }
 
-function stopGlide() {
+const stopGlide = () => {
   debug("glide stop");
   activity = STOP;
   Motion.stop();
 }
 
-function updateDrag(ev) {
+const updateDrag = ev => {
   debug("drag update");
-  let pos = evPos(ev);
-  mouseOrigin = vadd(mouseOrigin, Scroll.move(vsub(pos,mouseOrigin)));
+  const pos = evPos(ev);
+  mouseOrigin = vadd(mouseOrigin, Scroll.move(vsub(pos, mouseOrigin)));
   Motion.impulse(pos, ev.timeStamp);
 }
 
-function startDrag(ev) {
+const startDrag = ev => {
   debug("drag start");
   activity = DRAG;
   Scroll.start(ev);
   updateDrag(ev);
 }
 
-function stopDrag(ev) {
+const stopDrag = ev => {
   debug("drag stop");
   CoverDiv.hide();
   updateDrag(ev);
@@ -281,7 +282,7 @@ function stopDrag(ev) {
 
 // ===== Event handlers =====
 
-function onMouseDown(ev) {
+const onMouseDown = ev => {
   blockContextMenu = false;
   switch (activity) {
   //
@@ -328,7 +329,7 @@ function onMouseDown(ev) {
 }
 addEventListener("mousedown", onMouseDown, true);
 
-function onMouseMove(ev) {
+const onMouseMove = ev => {
   switch (activity) {
   //
   case STOP: case GLIDE: break;
@@ -341,7 +342,7 @@ function onMouseMove(ev) {
   //
   case CLICK:
     if (ev.buttons != cOptions.buttons) break;
-    if (vmag2(vsub(mouseOrigin,evPos(ev))) > 9) {
+    if (vmag2(vsub(mouseOrigin, evPos(ev))) > cOptions.sensitivity2) {
       if (options.button == 2) blockContextMenu = true;
       CoverDiv.show();
       startDrag(ev);
@@ -353,7 +354,7 @@ function onMouseMove(ev) {
 }
 addEventListener("mousemove", onMouseMove, true);
 
-function onMouseUp(ev) {
+const onMouseUp = ev => {
   switch (activity) {
   //
   case STOP: break;
@@ -376,12 +377,12 @@ function onMouseUp(ev) {
 }
 addEventListener("mouseup", onMouseUp, true);
 
-function onMouseOut(ev) {
+const onMouseOut = ev => {
   if (activity === DRAG && ev.toElement == null) stopDrag(ev);
 }
 addEventListener("mouseout", onMouseOut, true);
 
-function onContextMenu(ev) {
+const onContextMenu = ev => {
   if (!blockContextMenu) return;
   blockContextMenu = false;
   debug("blocking context menu");
